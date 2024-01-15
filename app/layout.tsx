c93b76d601085971bfd084e5d11bba60125465ed
Zachary Rizzuto <zachrizzuto@gmail.com>
2024-01-14 20:57:01 -0600

    Fix app for other computers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
-"use client";
+"use client"
 import { UserContentProvider } from "@/Components/Providers/UserContentProvider";
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Parry",
-  description: "Counter those calories!",
-  icons:
-    "https://as1.ftcdn.net/v2/jpg/02/22/72/16/1000_F_222721696_pBZvmzx6hW6hHHlV8K2KA0tLS2qVzwvf.jpg",
-};
+// export const metadata: Metadata = {
+//   title: "Parry",
+//   description: "Counter those calories!",
+//   icons:
+//     "https://as1.ftcdn.net/v2/jpg/02/22/72/16/1000_F_222721696_pBZvmzx6hW6hHHlV8K2KA0tLS2qVzwvf.jpg",
+// };
 
 export default function RootLayout({
   children,
@@ -21,12 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <UserContentProvider>
         <body className={inter.className}>
+          <UserContentProvider>
           <Toaster />
           {children}
+          </UserContentProvider>
         </body>
-      </UserContentProvider>
     </html>
   );
 }
